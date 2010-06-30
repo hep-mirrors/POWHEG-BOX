98d3e9f0892baf7831cad06ecb52270352296e40
oleari <oleari@9e129b38-c2c0-4eec-9301-dc54fda404de>
2010-06-30 22:10:33 +0000

    Z_plus_jet/nlegborn.h: recently introduced bug corrected
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX@82 9e129b38-c2c0-4eec-9301-dc54fda404de

--- Z_plus_jet/nlegborn.h
+++ Z_plus_jet/nlegborn.h
@@ -1,3 +1,5 @@
+c -*- Fortran -*-
+
 c The user must set nlegborn to the appropriate value for his process.
       integer nlegborn,nlegreal
       
@@ -7,10 +9,10 @@ c The user must set nlegborn to the appropriate value for his process.
 c     ndiminteg is the dimensionality of the full real integral
 c     ndiminteg=(nlegreal-2)*3-4+2-1
 c     if there are undecayed resonances, we need extra variables to pilot
-c     the resonance masses
+c     the resonance's masses
 
       integer ndiminteg
-      parameter (ndiminteg=(nlegreal-2)*3-4+2)
+      parameter (ndiminteg=(nlegreal-2)*3-4+2-1)
 
       integer maxprocborn,maxprocreal
       parameter (maxprocborn=200,maxprocreal=500)

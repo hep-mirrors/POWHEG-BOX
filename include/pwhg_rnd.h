917a2f03e0d6ee83372a88ed29c836cc827bb05e
nason <nason@9e129b38-c2c0-4eec-9301-dc54fda404de>
2010-09-07 08:29:36 +0000

    Forgot pwhg_rnd.h
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX@141 9e129b38-c2c0-4eec-9301-dc54fda404de

---
+++ include/pwhg_rnd.h
@@ -0,0 +1,6 @@
+c -*- fortran -*-
+      
+      integer rnd_numseeds,rnd_initialseed,rnd_iwhichseed
+      character * 4 rnd_cwhichseed
+      common/pwhg_rnd/rnd_numseeds,rnd_initialseed,rnd_iwhichseed,
+     1 rnd_cwhichseed

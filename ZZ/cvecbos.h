e4d601f8c9960a6d2c8cd88471d4eed01010ed69
nason <nason@9e129b38-c2c0-4eec-9301-dc54fda404de>
2011-05-07 05:03:48 +0000

    ZZ : added missing files setup-PYTHIA-lhef.f, setup-HERWIG-lhef.f and cvecbos.h
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX@483 9e129b38-c2c0-4eec-9301-dc54fda404de

---
+++ ZZ/cvecbos.h
@@ -0,0 +1,7 @@
+c -*- Fortran -*-
+
+      real * 8 vsymfact
+      integer idvecbos1,vdecaymode1,idvecbos2,vdecaymode2
+      common/cvecbos/vsymfact,idvecbos1,vdecaymode1,
+     1  idvecbos2,vdecaymode2
+

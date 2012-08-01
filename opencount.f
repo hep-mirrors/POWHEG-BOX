b9fcde893307b2e85a62f253a4c7002a6c1ec925
nason <nason@9e129b38-c2c0-4eec-9301-dc54fda404de>
2012-08-01 10:23:19 +0000

    Definitely fixing opencount for multiseed usage
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX@2065 9e129b38-c2c0-4eec-9301-dc54fda404de

--- opencount.f
+++ opencount.f
@@ -1,5 +1,6 @@
       subroutine opencount(maxev)
       implicit none
+      include 'pwhg_rnd.h'
       integer maxev
       character * 30 file
       character * 20 pwgprefix
@@ -8,32 +9,45 @@
       integer ios
       character * 7 string
       real * 8 powheginput
-      integer  lenocc,j,ifile
-      external powheginput,lenocc
-      integer nev
+      external powheginput
+      integer nev,j
       maxev=0
-      if(powheginput('#manyseeds').eq.1) then
-         write(*,*) 'enter file number'
-         read(*,*) ifile
-      else
-         ifile = -1
-      endif
-      if(ifile.eq.-1) then
-         file=pwgprefix(1:lprefix)//'events.lhe'
-      else
-         file=pwgprefix(1:lprefix)//'events-0000.lhe'
-         write(file(lprefix+8:lprefix+8),'(i4)') ifile
-         do j=lprefix+8,lprefix+11
-            if(file(j:j).eq.' ') file(j:j) = '0'
-         enddo
-      endif
+      file='pwgevents.lhe'
       open(unit=97,file=file,status='old',iostat=ios)
       if(ios.ne.0) then
-         write(*,*)' file not found:',file(1:lenocc(file))
+         do j=30,0,-1
+            if(file(j:j).ne.' ') exit
+         enddo
+         write(*,*)' file not found:',file(1:j)
          write(*,*)' enter name of event file'
          read(*,'(a)') file
-         open(unit=97,file=file,status='old')
+         open(unit=97,file=file,status='old',iostat=ios)
+         if(ios.ne.0) then
+            write(*,*) 'cannot open; aborting ...'
+            call exit(-1)
+         endif
+c get the name prefix
+         j=index(file,'events')
+         if(j.gt.1) then
+            pwgprefix=file(1:j-1)
+            lprefix=j-1
+         else
+            lprefix=0
+         endif
+c or a sequence number for manyseeds usage
+         if(file(j+6:j+6).eq.'-') then
+            rnd_cwhichseed=file(j+7:j+10)
+         else
+            rnd_cwhichseed='none'
+         endif
+      else
+         pwgprefix='pwg'
+         lprefix=3
+         rnd_cwhichseed='none'
       endif
+      write(*,*) 'prefix: "'//pwgprefix(1:lprefix)//'",', ' seed: "'//
+     1     rnd_cwhichseed//'"'
+c
       write(*,*) ' Opened event file ',file
       write(*,*) ' Counting events in ', file
       write(*,*) ' This may take some time...'

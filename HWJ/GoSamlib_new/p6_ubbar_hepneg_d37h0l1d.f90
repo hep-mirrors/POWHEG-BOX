1fc751a41683ee441b995fc709063fd6ecd8f795
luisonig <luisonig@9e129b38-c2c0-4eec-9301-dc54fda404de>
2013-02-19 12:19:57 +0000

    New virtuals added again.
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX@2189 9e129b38-c2c0-4eec-9301-dc54fda404de

---
+++ HWJ/GoSamlib_new/p6_ubbar_hepneg_d37h0l1d.f90
@@ -0,0 +1,106 @@
+module     p6_ubbar_hepneg_d37h0l1d
+   ! file:      /home/pcl305a/luisonig/Documents/GoSamPowheg/POWHEG-BOX/HWJ/GoSa
+   ! m/Virtual/p6_ubbar_hepneg/helicity0/d37h0l1d.f90
+   ! generator: haggies (1.1)
+   use p6_ubbar_hepneg_config, only: ki
+   use p6_ubbar_hepneg_util, only: cond, d => metric_tensor
+   
+
+   implicit none
+   private
+
+   complex(ki), parameter :: i_ = (0.0_ki, 1.0_ki)
+   integer, private :: iv0
+   integer, private :: iv1
+   real(ki), dimension(4), private :: qshift
+
+
+   public :: derivative, reconstruct_d37
+contains
+!---#[ function brack_1:
+   pure function brack_1(Q, mu2) result(brack)
+      use p6_ubbar_hepneg_model
+      use p6_ubbar_hepneg_kinematics
+      use p6_ubbar_hepneg_color
+      use p6_ubbar_hepneg_abbrevh0
+      implicit none
+      complex(ki), dimension(4), intent(in) :: Q
+      complex(ki), intent(in) :: mu2
+      complex(ki) :: brack
+      brack = (abb37n9+abb37n10*(e6(1)*qshift(1)-e6(2)*qshift(2)-e6(3)*qshift(3)&
+      &-e6(4)*qshift(4))+abb37n11*(spvak1k6(1)*qshift(1)-spvak1k6(2)*qshift(2)-s&
+      &pvak1k6(3)*qshift(3)-spvak1k6(4)*qshift(4))+abb37n12*(spvae6k1(1)*qshift(&
+      &1)-spvae6k1(2)*qshift(2)-spvae6k1(3)*qshift(3)-spvae6k1(4)*qshift(4))+abb&
+      &37n13*(spvak6k1(1)*qshift(1)-spvak6k1(2)*qshift(2)-spvak6k1(3)*qshift(3)-&
+      &spvak6k1(4)*qshift(4))+abb37n14*dotproduct(k6, qshift)+abb37n15*dotproduc&
+      &t(k1, qshift))
+   end  function brack_1
+!---#] function brack_1:
+!---#[ function brack_2:
+   pure function brack_2(Q, mu2) result(brack)
+      use p6_ubbar_hepneg_model
+      use p6_ubbar_hepneg_kinematics
+      use p6_ubbar_hepneg_color
+      use p6_ubbar_hepneg_abbrevh0
+      implicit none
+      complex(ki), dimension(4), intent(in) :: Q
+      complex(ki), intent(in) :: mu2
+      complex(ki) :: brack
+      brack = (e6(iv1)*abb37n10+k1(iv1)*abb37n15+k6(iv1)*abb37n14+spvae6k1(iv1)*&
+      &abb37n12+spvak1k6(iv1)*abb37n11+spvak6k1(iv1)*abb37n13)
+   end  function brack_2
+!---#] function brack_2:
+
+!---#[ function derivative:
+   function derivative(mu2, i1) result(numerator)
+      use p6_ubbar_hepneg_globalsl1, only: epspow
+      use p6_ubbar_hepneg_kinematics
+      use p6_ubbar_hepneg_abbrevh0
+      implicit none
+      complex(ki), intent(in) :: mu2
+      integer, intent(in), optional :: i1
+      complex(ki) :: numerator
+      complex(ki) :: loc
+      integer :: t1
+      integer :: deg
+      complex(ki), dimension(4), parameter :: Q = (/&
+        &(0.0_ki,0.0_ki),(0.0_ki,0.0_ki),(0.0_ki,0.0_ki),(0.0_ki,0.0_ki)/)
+      qshift = -k3-k5-k4
+      numerator = 0.0_ki
+      deg = 0
+      if(present(i1)) then
+         iv1 = i1
+         deg = 1
+      else
+         iv1 = 1
+      end if
+      t1 = 0
+      if(deg.eq.0) then
+         numerator = (cond(epspow.eq.t1,brack_1,Q,mu2))
+         return
+      end if
+      if(deg.eq.1) then
+         numerator = (cond(epspow.eq.t1,brack_2,Q,mu2))
+         return
+      end if
+   end function derivative
+!---#] function derivative:
+!---#[ subroutine reconstruct_d37:
+   subroutine     reconstruct_d37(coeffs)
+      use p6_ubbar_hepneg_groups, only: tensrec_info_group3
+      implicit none
+      complex(ki), parameter :: czip = (0.0_ki, 0.0_ki)
+      complex(ki), parameter :: cone = (1.0_ki, 0.0_ki)
+
+      type(tensrec_info_group3), intent(out) :: coeffs
+      ! rank 1 case:
+      !---[# reconstruct coeffs%coeffs_37:
+      coeffs%coeffs_37%c0 = derivative(czip)
+      coeffs%coeffs_37%c1(1,1) = derivative(czip,1)
+      coeffs%coeffs_37%c1(2,1) = -derivative(czip,2)
+      coeffs%coeffs_37%c1(3,1) = -derivative(czip,3)
+      coeffs%coeffs_37%c1(4,1) = -derivative(czip,4)
+      !---#] reconstruct coeffs%coeffs_37:
+   end subroutine reconstruct_d37
+!---#] subroutine reconstruct_d37:
+end module p6_ubbar_hepneg_d37h0l1d

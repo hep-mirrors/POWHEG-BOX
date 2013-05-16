af1c2c8dc5538b3717f970144e057c42d56206ca
nason <nason@9e129b38-c2c0-4eec-9301-dc54fda404de>
2013-05-16 09:31:30 +0000

    Take away write_counters routine (now in cernroutines.f)
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX@2288 9e129b38-c2c0-4eec-9301-dc54fda404de

--- ReweightingStuff/pwhg_main.f
+++ ReweightingStuff/pwhg_main.f
@@ -184,22 +184,3 @@ c in the powheg.input file; useful to catch mispelled keywords
       tmp=powheginput('print unused tokens')
       end
 
-      subroutine write_counters
-      implicit none
-      include 'pwhg_rnd.h'
-      integer iun
-      character * 20 pwgprefix
-      integer lprefix
-      common/cpwgprefix/pwgprefix,lprefix
-      call newunit(iun)
-      if(rnd_cwhichseed.eq.'none') then
-         open(unit=iun,file=pwgprefix(1:lprefix)//'counters.dat'
-     1     ,status='unknown')
-      else
-         open(unit=iun,file=pwgprefix(1:lprefix)//'counters'//
-     1        rnd_cwhichseed//'.dat',status='unknown')
-      endif
-      call printcnt(iun)
-      close(iun)
-      end
-

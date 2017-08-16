11eadd35991d6cef7359419d43a0fd21e31049fb
oleari <oleari@9e129b38-c2c0-4eec-9301-dc54fda404de>
2017-08-16 13:34:56 +0000

    Fixed a bug in the virtual amplitudes computed by MCFM These amplitudes are already in CDR, so no need to perform the DR -> CDR conversion
    git-svn-id: svn://powhegbox.mib.infn.it/trunk/POWHEG-BOX@3443 9e129b38-c2c0-4eec-9301-dc54fda404de

--- HJ/virtual.f
+++ HJ/virtual.f
@@ -17,8 +17,12 @@ c Virtual needs to be provided by the user and put here
 
       virtual_DR=res(0)/(st_alpha/(2*pi))
 
-c from dimensional reduction to dimensional regularization
-      call from_DR_to_CDR(p,vflav,virtual_DR,virtual)
+c     The virtual contribution computed by MCFM is ALREADY in CDR, and NOT in DR, as
+c     previously thought. So no need to DR -> CDR conversion
+c$$$  
+c$$$c from dimensional reduction to dimensional regularization
+c$$$      call from_DR_to_CDR(p,vflav,virtual_DR,virtual)
+      virtual = virtual_DR
 
 
 cc The following code is used to check the double pole
